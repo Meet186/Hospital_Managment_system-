3935cb1d0a82a846e2f4d213c6a237c4d2bcfa59
Meet Solanki <meetsolanki7974@gmail.com>
2025-10-23 13:30:32 +0530

    added Reception and some changes in conn class

--- src/hospital/managment/system/login.java
+++ src/hospital/managment/system/login.java
@@ -2,8 +2,11 @@ package  hospital.managment.system;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 
-public class login extends JFrame {
+public class login extends JFrame implements ActionListener {
 
     JTextField textField;
     JPasswordField passwordField;
@@ -54,6 +57,7 @@ public class login extends JFrame {
         b1.setFont(new Font("serif",Font.BOLD,15));
         b1.setBackground(Color.BLACK);
         b1.setForeground(Color.WHITE);
+        b1.addActionListener(this);
         add(b1);
 
         b2 = new JButton("Cancel");
@@ -61,6 +65,7 @@ public class login extends JFrame {
         b2.setFont(new Font("serif",Font.BOLD,15));
         b2.setBackground(Color.BLACK);
         b2.setForeground(Color.WHITE);
+        b2.addActionListener(this);
         add(b2);
 
         // content that create a frame
@@ -73,7 +78,30 @@ public class login extends JFrame {
     }
     public static void main(String[] args) {
         new login();
-
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == b1){
+            try {
+                conn  c = new conn();
+                String user = textField.getText();
+                String pass = passwordField.getText();
+
+                String q = "select * from login where ID = '"+user+"' and PW = '"+pass+"'";
+                ResultSet resultSet = c.statement.executeQuery(q);
+
+                if(resultSet.next()){
+                    new Reception();
+                    setVisible(false);
+                } else {
+                    JOptionPane.showMessageDialog(null,"Invalid");
+                }
+            } catch (Exception E){
+                E.printStackTrace();
+            }
+        } else {
+            System.exit(10);
+        }
+    }
 }
